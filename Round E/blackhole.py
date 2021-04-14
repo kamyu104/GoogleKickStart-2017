b2b9ade76576d12b450ae76117b09b43aa3c6a43
Allen Liu <kamyu104@gmail.com>
2021-04-14 15:51:23 +0800

    update

--- Round E/blackhole.py
+++ Round E/blackhole.py
@@ -61,7 +61,7 @@ def rotate_to_xy_plane(points):
     theta = angle(v, [0, 0, 1], [1, 0, 0])  # if v is zero vector, theta will be 0
     matrix = rotate_x(matrix, cos(theta), sin(theta))  # rotate from v to z-axis by theta
     v = list(matrix[0])
-    theta = angle(v, [0, 0, 1], [0, 1, 0]) # if v is zero vector, theta will be 0
+    theta = angle(v, [0, 0, 1], [0, 1, 0])  # if v is zero vector, theta will be 0
     matrix = rotate_y(matrix, cos(theta), sin(theta))  # rotate from v to z-axis by theta
     return [[x, y] for x, y, _ in matrix[1:]]
 
